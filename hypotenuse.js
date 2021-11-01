259737e3a94594721172949b5691afff27dd953a
BISINIGISHIVANI <90177603+BISINIGISHIVANI@users.noreply.github.com>
2021-11-01 15:04:59 +0530

    hypotenuse calculated

---
+++ hypotenuse.js
@@ -0,0 +1,18 @@
+var sides=document.querySelectorAll(".side-input");
+var checkHypotenuseBtn=document.querySelector('#hypotenuse-btn');
+var outputL3=document.querySelector('#output');
+
+
+
+function calculateSumOfSides(a,b){
+    const sumOfSides=a*a+b*b;
+    return sumOfSides;
+}
+
+function calculateHypotenuseOfTriangle(){
+    const sumOfSides=calculateSumOfSides(Number(sides[0].value),Number(sides[1].value));
+    const hypotenuse=Math.sqrt(sumOfSides);
+outputL3.innerText="The length of the hypotenuse is "+hypotenuse+"cm";
+}
+
+checkHypotenuseBtn.addEventListener("click",calculateHypotenuseOfTriangle);
