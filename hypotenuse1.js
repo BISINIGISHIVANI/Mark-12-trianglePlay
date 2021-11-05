e03667a4540b7df38967540b2530bd74c5de6692
BISINIGISHIVANI <90177603+BISINIGISHIVANI@users.noreply.github.com>
2021-11-05 15:52:11 +0530

    innerText id changed

--- hypotenuse1.js
+++ hypotenuse1.js
@@ -1,16 +1,15 @@
 var sides=document.querySelectorAll(".side-input");
 var checkHypotenuseBtn=document.querySelector('#hypotenuse-btn');
-var outputL3=document.querySelector('#output');
+var outputL3=document.querySelector("#textOutput");
 
-function calculateSumOfSides(a,b){
-    const sumOfSides=a*a+b*b;
-    return sumOfSides;
-}
 
-function calculateHypotenuseOfTriangle(){
-    const sumOfSides=calculateSumOfSides(Number(sides[0].value),Number(sides[1].value));
-    const hypotenuse=Math.sqrt(sumOfSides);
-    outputL3.innerText="The length of the hypotenuse is "+hypotenuse;
-}
+function add(){
+    let sumSides=Math.pow(Number(sides[0].value),2)+Math.pow(Number(sides[1].value),2);
+   
+    let hypotenuseResult=Math.sqrt(sumSides);
+    console.log("clicked",hypotenuseResult);
+    outputL3.innerText=`Length of Hypotenuse is:  ${sumSides}  cm.`;
+    
+};
 
-checkHypotenuseBtn.addEventListener("click",calculateSumOfSides);
+checkHypotenuseBtn.addEventListener("click",add);
